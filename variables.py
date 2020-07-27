dd833842c01fefddb77554d857d8ffd4d33d8cf5
lefousam-prog <61300076+lefousam-prog@users.noreply.github.com>
2020-07-27 03:06:11 -0600

    codigo python

---
+++ variables.py
@@ -0,0 +1,43 @@
+Python 3.6.4 (v3.6.4:d48eceb, Dec 19 2017, 06:54:40) [MSC v.1900 64 bit (AMD64)] on win32
+Type "copyright", "credits" or "license()" for more information.
+>>> #tipos de variables
+>>> num1=3
+>>> num1
+3
+>>> print(num1)
+3
+>>> letras="hola mundo"
+>>> letras
+'hola mundo'
+>>> print(letras)
+hola mundo
+>>> #imprimir un texto con saltos
+>>> palabra="""hola
+que
+tal
+estas"""
+>>> print(palabra)
+hola
+que
+tal
+estas
+>>> palabra
+'hola\nque\ntal\nestas'
+>>> #hacer saltos de lineas de codigo con black slash
+>>> palabra="""dime que tal\
+estas en este lugar\
+distres"""
+>>> print(print)
+<built-in function print>
+>>> print(palabra)
+dime que talestas en este lugardistres
+>>> #sacar el tipo a una variable
+>>> num1=2
+>>> type(num1)
+<class 'int'>
+>>> num1=2.5
+>>> type(num1)
+<class 'float'>
+>>> type(palabra)
+<class 'str'>
+>>> 
